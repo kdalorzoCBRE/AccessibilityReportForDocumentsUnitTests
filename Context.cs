b1a69b22ad230e185ad48af05444f828ca2ae26b
Karla Dalorzo Vargas <karla.dalorzo@cbre.com>
2024-01-26 09:35:16 -0600

    Unit Tests: the beginning

---
+++ Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AccessibilityReportForDocumentsTests
+{
+    public static class Context
+    {
+        public static ILogger ContextLogger()
+        {
+            return new LoggerFactory().CreateLogger(typeof(Context));
+        }
+    }
+}
