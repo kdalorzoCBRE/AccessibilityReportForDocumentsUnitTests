ed2b024432a7f6b7798964b98500379aecd5e26a
Karla Dalorzo Vargas <karla.dalorzo@cbre.com>
2024-01-29 14:34:18 -0600

    New Scan for Table Headers

---
+++ core/scanners/presentationScanners/PresentationObjectHeaderScanner.cs
@@ -0,0 +1,40 @@
+﻿using AccessibilityReportForDocuments.core.errors;
+using AccessibilityReportForDocuments.core.scanners.wordScanners;
+using AccessibilityReportForDocumentsTests;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using Xunit.Abstractions;
+
+namespace AccessibilityReportForDocuments.tests.core.scanners.presentationScanners
+{
+    public class PresentationObjectHeaderScannerTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public PresentationObjectHeaderScannerTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void ScanPresentationTableHeaderScanner()
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            // Given
+            PresentationTableHeaderScanner scanner = new(Context.ContextLogger());
+            string document = "C:\\Users\\v-karladal\\source\\repos\\AccessibilityReportForDocumentsTests\\mocks\\presentation table header and not.pptx";
+
+            // When
+            using PresentationDocument presentationDocument = PresentationDocument.Open(document, false);
+            Presentation presentation = presentationDocument.PresentationPart.Presentation;
+            List<AccessibilityError> result = scanner.Scan(presentationDocument, presentation);
+
+            // Then            
+            this.output.WriteLine(output.ToString());
+            Assert.Equal(1, result.Count);
+            Assert.Equal("Table 3", result[0].ObjectName);
+        }       
+    }
+}
---
+++ core/scanners/wordScanners/WordObjectHeaderScanner.cs
@@ -0,0 +1,32 @@
+﻿using AccessibilityReportForDocuments.core.errors;
+using AccessibilityReportForDocuments.core.scanners.wordScanners;
+using AccessibilityReportForDocumentsTests;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace AccessibilityReportForDocuments.tests.core.scanners.presentationScanners
+{
+    public class WordObjectHeaderScannerTests
+    {
+
+        [Fact]
+        public void ScanWordTableHeaderScannerErrorsFound()
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            // Given
+            WordTableHeaderScanner scanner = new(Context.ContextLogger());
+            string document = "C:\\Users\\v-karladal\\source\\repos\\AccessibilityReportForDocumentsTests\\mocks\\doc with tables header no header.docx";
+
+            // When
+            using WordprocessingDocument wordDocument = WordprocessingDocument.Open(document, false);
+            Body body = wordDocument.MainDocumentPart.Document.Body;
+            List<AccessibilityError> result = scanner.Scan(wordDocument, body);
+
+            // Then            
+            Assert.Single(result);
+            Assert.Equal("Table 0480", result[0].ObjectName);
+        }       
+    }
+}
